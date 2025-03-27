b141336c961eb44c532217db2819ee11a44e07b6
Tzvi Rolnik <tzvirol@std.hac.ac.il>
2025-03-27 23:19:25 +0200

    class comp dont working wall i nedd to fix it

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -27,7 +27,7 @@ void Manager::showMenu()
 	for (int i = 0; i < m_operations.size(); i++) {
 		cout << i << ". " << m_operations[i]->getName() << endl;
 	}
-	cout << "Enter command ('help' for the list of available commands) : \n";
+	cout << "Enter command ('help' for the list of available commands) : ";
 
 }
 //-------------------------------------------
@@ -43,9 +43,27 @@ void Manager::processCommand(std::string& command, int& num1, int& num2)
 	
 	 if (command == "exit") {
 		m_need2exitFromLoop = true;
-	    }
-	 else if (command == "eval") {
-	 
 	 }
+	 else if (command == "eval") {
+		 m_eval.calculate(m_operations.at(num1), num2);
+	 }
+	 else if (command == "scal") {
+		 m_operations.push_back(std::make_shared<Scal>(num1));
+	 }
+
+	 else if (command == "comp") {
+		 m_operations.push_back(std::make_shared<Comp>(m_operations.at(num1), m_operations.at(num2)));
+	 }
+	 else if (command == "add") {
+		 m_operations.push_back(std::make_shared<Add>(m_operations.at(num1), m_operations.at(num2)));
+	 }
+	 else if (command == "sub") {
+		 m_operations.push_back(std::make_shared<Sub>(m_operations.at(num1), m_operations.at(num2)));
+	 }
+	 else if (command == "del") {
+		 m_operations.erase(m_operations.begin() + num1);
+	 }
+	 else 
+		 cout << "Unknown command\n";
 	
 }
