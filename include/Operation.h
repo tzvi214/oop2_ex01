497522502956a93a01d6e17c0a829e923538fbbd
Tzvi Rolnik <tzvirol@std.hac.ac.il>
2025-04-03 18:43:39 +0300

    i made the function calc in the base (class Operation) pure virtual

--- include/Operation.h
+++ include/Operation.h
@@ -10,11 +10,10 @@ class Operation
 public:
 	 Operation(string, int);
 	virtual ~Operation() = default;
-	virtual SqrMatrix calc(std::vector<SqrMatrix>) { return SqrMatrix{}; }
+	virtual SqrMatrix calc(std::vector<SqrMatrix>) = 0; // pure virtual function to be implemented by derived classes.
 
 	virtual int getMatrixRequired() const { return m_matrixRequired; }; // return the number of matrix required for the operation (virtual).
-
-	 string getName() const { return m_name; }; // return the name of the class (virtual).
+	 string getName() const { return m_name; }; // return the name of the class
 protected:
 	string m_name;
 	int m_matrixRequired;
